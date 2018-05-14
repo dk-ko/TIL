e5c156d4c957f24c77c5d9535041b8145a75b468
Dakyung Ko <kdkyung57@gmail.com>
2018-05-15 03:37:36 +0900

    feat: Handling logic errors

--- Spring/form1/src/main/java/net/skhu/controller/UserController.java
+++ Spring/form1/src/main/java/net/skhu/controller/UserController.java
@@ -1,5 +1,7 @@
 package net.skhu.controller;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,10 +28,30 @@ public class UserController {
 		model.addAttribute("departments", departmentRepository.findAll());
 		return "user/register";
 	}
+	
+	private boolean isValid(UserRegistrationModel userModel, BindingResult bindingResult) {
+		if(userModel.getPasswd1().equals(userModel.getPasswd2()) == false) {
+			bindingResult.rejectValue("password2", null, "비밀번호가 일치하지 않습니다.");
+			return false;
+		}
+		/*
+		Optional<User> result = userRepository.findById(Integer.parseInt(userModel.getUserid()));
+		if(!result.isPresent()) {
+			bindingResult.rejectValue("userid", null,"사용자 아이디가 중복됩니다.");
+			return false;
+		}
+		*/
+		User user = userRepository.findByUserid(userModel.getUserid());
+		if(user != null) {
+			bindingResult.rejectValue("userid", null, "사용자 아이디가 중복됩니다.");
+			return false;
+		}
+		return true;
+	}
 
 	@RequestMapping(value="register", method=RequestMethod.POST)
 	public String register(@Valid UserRegistrationModel userModel, BindingResult bindingResult, Model model) {
-		if (bindingResult.hasErrors()) {
+		if (bindingResult.hasErrors() || isValid(userModel, bindingResult)==false) {
 			model.addAttribute("departments", departmentRepository.findAll());
 			return "user/register";
 		}
